38418d294baec86d133e6f50f0f4b29a8a0335cc
Mukesh Burdak <mburdak626@gmail.com>
2026-03-07 17:11:59 +0530

    Fix condition to check for non-positive numbers

--- Ugly Number/1.java
+++ Ugly Number/1.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isUgly(int n) {
         boolean result = false;
         int []fact = {2,3,5};
-        if (n < 0) {
+        if (n <= 0) {
             return false;
         }
        for (int i : fact) {
